1117fd8d60a8cf27be12746ca6ca37f56e91a868
RemiPHILIPPOT <82380347+RemiPHILIPPOT@users.noreply.github.com>
2023-11-13 15:05:29 +0100

    create header and cards projects and setup fakebackend

---
+++ components/Header.tsx
@@ -0,0 +1,63 @@
+// Header.tsx
+import React, { useEffect, useState } from "react";
+import "../src/styles/header.css";
+import { Link, useNavigate } from "react-router-dom";
+import LogoRp from "../src/logo/logo_rp.png";
+
+interface Author {
+    firstName: string;
+    lastName: string;
+    description: string;
+}
+
+const Header: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = (to: string) => {
+        navigate(to);
+    };
+
+    const [author, setAuthor] = useState<Author | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/projects/projects.json");
+                const data = await response.json();
+                console.log("Data from JSON:", data);
+                setAuthor(data.author);
+            } catch (error) {
+                console.error("Error fetching author:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <header className="header">
+            <Link to="/">
+                <img className="logo" src={LogoRp} alt="Logo Rémi PHILIPPOT" />
+            </Link>
+            <nav className="header_nav">
+                <ul>
+                    <li className="header_link">
+                        <Link to="/">Accueil</Link>
+                    </li>
+                    <li className="header_link">
+                        <Link to="/projects">Projets</Link>
+                    </li>
+                    {/* Ajoutez d'autres liens selon votre besoin */}
+                </ul>
+            </nav>
+            {author && (
+                <div className="author-info">
+                    <h2>{`${author.firstName} ${author.lastName}`}</h2>
+                    <p>{author.description}</p>
+                </div>
+            )}
+        </header>
+    );
+};
+
+export default Header;
---
+++ components/ProjectList.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import "../src/styles/projectList.css";
+
+interface Project {
+    id: number;
+    title: string;
+    description: string;
+    technologies: string[];
+    image: string;
+    link: string;
+}
+
+const ProjectList: React.FC = () => {
+    const [projects, setProjects] = useState<Project[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/projects/projects.json");
+                const data = await response.json();
+                console.log("Data from JSON:", data);
+                setProjects(data.projects);
+            } catch (error) {
+                console.error("Error fetching projects:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const ProjectCard: React.FC<Project> = ({
+        id,
+        title,
+        description,
+        technologies,
+        image,
+        link,
+    }) => {
+        const [showFullContent, setShowFullContent] = useState(false);
+
+        const toggleContent = () => {
+            setShowFullContent(!showFullContent);
+        };
+
+        return (
+            <div
+                className={`project-card ${showFullContent ? "expanded" : ""}`}
+                key={id}
+            >
+                <h3>{title}</h3>
+                <p className="description">
+                    {showFullContent
+                        ? description
+                        : `${description.substring(0, 100)}...`}
+                </p>
+                <p className="technologies">
+                    Technologies : {technologies.join(", ")}
+                </p>
+                <img src={image} alt={`Capture d'écran de ${title}`} />
+                {!showFullContent && (
+                    <button className="see-more" onClick={toggleContent}>
+                        Voir plus
+                    </button>
+                )}
+                {showFullContent && (
+                    <button className="see-less" onClick={toggleContent}>
+                        Voir moins
+                    </button>
+                )}
+                <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="project-link"
+                >
+                    Voir le projet
+                </a>
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            <h2>Mes Projets</h2>
+            <div className="project-container">
+                {projects.map((project) => (
+                    <ProjectCard key={project.id} {...project} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ProjectList;
